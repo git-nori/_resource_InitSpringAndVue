40a1919d546a8688db407e24542dfb4c1606b234
HironoriSato <maza.hs5360@gmail.com>
2019-10-12 17:05:43 +0900

    [add]frontendのサーバーからbackendのサーバーにアクセスする際のCORSによるエラーの対応を設定

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+	"transpileDependencies" : [ "vuetify" ],
+	/*
+	 * changeOrigin: true
+	 * CORS(オリジンが違うことによるエラー)の防止
+	 * backend 側で @RestController アノテーションを付与したクラスに@CrossOrigin アノテーションを付与して設定しなくてもアクセスできるように
+	 */
+	devServer : {
+		proxy : {
+			"^/webapi/sample" : {
+				target : "http://localhost:8081",
+				changeOrigin : true
+			}
+		}
+	}
 }
